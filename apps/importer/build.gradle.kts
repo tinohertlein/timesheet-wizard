39d4e89a44ca592df720a084b0e1a1ff43782746
tinohertlein <tino@hertlein.dev>
2024-02-06 18:21:27 +0100

    chore: revert Micronaut update

--- apps/importer/build.gradle.kts
+++ apps/importer/build.gradle.kts
@@ -10,7 +10,7 @@ plugins {
     id("org.jetbrains.kotlin.kapt") version kotlin
     id("org.jetbrains.kotlin.plugin.allopen") version kotlin
     id("com.github.johnrengelman.shadow") version "8.1.1"
-    id("io.micronaut.application") version "4.3.1"
+    id("io.micronaut.application") version "4.2.1"
     id("io.gitlab.arturbosch.detekt") version "1.23.5"
 }
 
