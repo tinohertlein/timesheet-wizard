b43fdbf084f85773b3205158a2435d063aa1e2e9
tinohertlein <tino@hertlein.dev>
2024-02-10 14:19:24 +0100

    chore: update dependencies

--- apps/importer/build.gradle.kts
+++ apps/importer/build.gradle.kts
@@ -29,7 +29,7 @@ dependencies {
     val kotlinLoggingVersion = "3.0.5"
     val jacksonVersion = "2.16.1"
     val logbackVersion = "1.4.14"
-    val awsSdkVersion = "2.23.18"
+    val awsSdkVersion = "2.23.21"
     val awsLambdaVersion = "3.11.4"
 
     annotationProcessor("io.micronaut.validation:micronaut-validation-processor")
@@ -57,7 +57,7 @@ dependencies {
 
     val junitVersion = "5.10.2"
     val mockServerVersion = "5.15.0"
-    val testContainersVersion = "1.19.4"
+    val testContainersVersion = "1.19.5"
     val assertJVersion = "3.25.3"
     val mockkVersion = "1.13.9"
 
