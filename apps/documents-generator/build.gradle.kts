c306c8bf3d6fece905fe05c8ac114521d657825c
tinohertlein <tino@hertlein.dev>
2024-06-02 16:40:59 +0200

    chore: update dependencies
    Signed-off-by: tinohertlein <tino@hertlein.dev>

--- apps/documents-generator/build.gradle.kts
+++ apps/documents-generator/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
     val kotlin = "2.0.0"
     kotlin("jvm") version kotlin
     kotlin("plugin.allopen") version kotlin
-    id("io.quarkus") version "3.10.1"
+    id("io.quarkus") version "3.11.0"
     id("io.gitlab.arturbosch.detekt") version "1.23.6"
 }
 
@@ -34,7 +34,7 @@ repositories {
 
 val quarkusPlatformGroupId = "io.quarkus.platform"
 val quarkusPlatformArtifactId = "quarkus-bom"
-val quarkusPlatformVersion = "3.10.1"
+val quarkusPlatformVersion = "3.11.0"
 
 dependencies {
     val guavaVersion = "33.2.0-jre"
