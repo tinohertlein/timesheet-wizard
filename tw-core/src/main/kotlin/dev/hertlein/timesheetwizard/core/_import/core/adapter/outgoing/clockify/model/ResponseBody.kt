0a468b7a8db3898121d2f0309055436650e3f655
Tino Hertlein <tino@hertlein.dev>
2024-11-12 17:15:44 +0100

    chore(deps): bump com.fasterxml.jackson.module:jackson-module-kotlin

--- tw-core/src/main/kotlin/dev/hertlein/timesheetwizard/core/_import/core/adapter/outgoing/clockify/model/ResponseBody.kt
+++ tw-core/src/main/kotlin/dev/hertlein/timesheetwizard/core/_import/core/adapter/outgoing/clockify/model/ResponseBody.kt
@@ -1,21 +1,23 @@
 package dev.hertlein.timesheetwizard.core._import.core.adapter.outgoing.clockify.model
 
-internal data  class ResponseBody(
+internal data class ResponseBody(
     val timeentries: List<TimeEntry>,
     val totals: List<Totals>? = null
 ) {
+    internal data class Totals(val totalTime: Long, val entriesCount: Long)
 
-   internal data  class Totals(val totalTime: Long, val entriesCount: Long)
-
-   internal data  class TimeEntry(
+    internal data class TimeEntry(
         val projectName: String,
         val description: String,
         val tags: List<Tag>?,
         val timeInterval: TimeInterval
     ) {
 
-       internal data  class Tag(val name: String)
+        internal data class Tag(val name: String) {
+            // Empty constructor is needed for com.fasterxml.jackson.module:jackson-module-kotlin:2.18.1
+            constructor() : this("")
+        }
 
-       internal data  class TimeInterval(val start: String, val end: String, val duration: Long)
+        internal data class TimeInterval(val start: String, val end: String, val duration: Long)
     }
 }
