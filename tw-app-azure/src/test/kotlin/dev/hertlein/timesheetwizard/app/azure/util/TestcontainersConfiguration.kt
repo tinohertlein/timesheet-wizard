7e616c1b12a85dd5f57b3acf23fb6ab19716a8da
Tino Hertlein <tino@hertlein.dev>
2025-04-21 11:09:27 +0200

    style: reformat TestcontainersConfiguration.kt

--- tw-app-azure/src/test/kotlin/dev/hertlein/timesheetwizard/app/azure/util/TestcontainersConfiguration.kt
+++ tw-app-azure/src/test/kotlin/dev/hertlein/timesheetwizard/app/azure/util/TestcontainersConfiguration.kt
@@ -23,10 +23,8 @@ class TestcontainersConfiguration {
 
     @Bean
     @Profile(TESTCONTAINERS)
-    fun blobServiceClientBuilder(
-        azureContainer: GenericContainer<*>,
-    ): BlobServiceClientBuilder {
-        val blobPort = azureContainer.getMappedPort(AZURE_CONTAINER_PORT)
+    fun blobServiceClientBuilder(azuriteContainer: GenericContainer<*>): BlobServiceClientBuilder {
+        val blobPort = azuriteContainer.getMappedPort(AZURE_CONTAINER_PORT)
         return BlobServiceClientBuilder()
             .connectionString("DefaultEndpointsProtocol=http;AccountName=devstoreaccount1;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;BlobEndpoint=http://127.0.0.1:$blobPort/devstoreaccount1;")
     }
