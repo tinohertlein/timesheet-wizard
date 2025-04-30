78d8832a1ad996eb43e31b129c2532ac10fd6cb8
Tino Hertlein <tino@hertlein.dev>
2025-04-30 21:14:36 +0200

    feat: set content type explicitly in Clockify HTTP request

--- tw-core/src/main/kotlin/dev/hertlein/timesheetwizard/core/_import/adapter/outgoing/clockify/report/ReportClient.kt
+++ tw-core/src/main/kotlin/dev/hertlein/timesheetwizard/core/_import/adapter/outgoing/clockify/report/ReportClient.kt
@@ -2,6 +2,8 @@ package dev.hertlein.timesheetwizard.core._import.adapter.outgoing.clockify.repo
 
 import com.google.common.net.HttpHeaders
 import dev.hertlein.timesheetwizard.core._import.adapter.outgoing.clockify.config.ClockifyConfig
+import org.springframework.http.MediaType.APPLICATION_JSON
+import org.springframework.http.MediaType.APPLICATION_JSON_VALUE
 import org.springframework.stereotype.Component
 import org.springframework.web.client.RestClient
 import org.springframework.web.util.UriComponentsBuilder
@@ -23,8 +25,9 @@ internal class ReportClient(
         return restClient
             .post()
             .uri(uri)
+            .contentType(APPLICATION_JSON)
             .body(requestBody)
-            .header(HttpHeaders.ACCEPT, "application/json")
+            .header(HttpHeaders.ACCEPT, APPLICATION_JSON_VALUE)
             .header("X-Api-Key", clockifyConfig.apiKey)
             .retrieve()
             .body(ResponseBody::class.java)
