92e9a546e5e933f4710cb2dff7b11cea885fd743
Tino Hertlein <tino@hertlein.dev>
2026-01-03 16:55:02 +0100

    chore(deps): upgrade to Spring Boot 4 and related dependencies; add custom `cloudFunctionJsonMapper` for compatibility

--- tw-app-azure/src/main/kotlin/dev/hertlein/timesheetwizard/app/azure/TwAzureApplication.kt
+++ tw-app-azure/src/main/kotlin/dev/hertlein/timesheetwizard/app/azure/TwAzureApplication.kt
@@ -1,15 +1,33 @@
 package dev.hertlein.timesheetwizard.app.azure
 
-import dev.hertlein.timesheetwizard.core.importing.domain.service.ImportService
 import dev.hertlein.timesheetwizard.core.anticorruption.Core
+import dev.hertlein.timesheetwizard.core.importing.domain.service.ImportService
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
+import org.springframework.cloud.function.json.JacksonMapper
+import org.springframework.cloud.function.json.JsonMapper
 import org.springframework.context.annotation.Bean
+import tools.jackson.core.StreamReadFeature
+import tools.jackson.databind.DeserializationFeature
+import tools.jackson.databind.SerializationFeature
 
 @SpringBootApplication(scanBasePackages = ["dev.hertlein.timesheetwizard"])
 @EnableConfigurationProperties(AzureClockifyConfig::class)
 class TwAzureApplication {
+
+    @Bean
+    fun cloudFunctionJsonMapper(builder: tools.jackson.databind.json.JsonMapper.Builder): JsonMapper {
+        // TODO remove when https://github.com/spring-cloud/spring-cloud-function/issues/1319 is fixed
+        val jsonMapper =
+            builder
+                .configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false)
+                .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+                .configure(DeserializationFeature.FAIL_ON_NULL_FOR_PRIMITIVES, false)
+                .configure(StreamReadFeature.INCLUDE_SOURCE_IN_LOCATION, false)
+                .build()
+        return JacksonMapper(jsonMapper)
+    }
 
     @Bean
     fun importService(repository: AzureBlobStorageRepository, clockifyConfig: AzureClockifyConfig): ImportService {
