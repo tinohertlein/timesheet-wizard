bf09e38c2ad67e4c5c7829b675b2b19bb1729bcc
Tino Hertlein <tino@hertlein.dev>
2026-03-23 21:22:08 +0100

    test: update Testcontainers dependencies to version 2.0.4 and adjust imports/configuration

--- tw-app-aws/src/test/kotlin/dev/hertlein/timesheetwizard/app/aws/util/TestcontainersConfiguration.kt
+++ tw-app-aws/src/test/kotlin/dev/hertlein/timesheetwizard/app/aws/util/TestcontainersConfiguration.kt
@@ -1,7 +1,6 @@
 package dev.hertlein.timesheetwizard.app.aws.util
 
-import org.testcontainers.containers.localstack.LocalStackContainer
-import org.testcontainers.utility.DockerImageName
+import org.testcontainers.localstack.LocalStackContainer
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider
 import software.amazon.awssdk.regions.Region
@@ -10,17 +9,18 @@ import software.amazon.awssdk.services.s3.S3Client
 object TestcontainersConfiguration {
 
     fun localStackContainer(): LocalStackContainer {
-        return LocalStackContainer(DockerImageName.parse("localstack/localstack:s3-latest"))
+        return LocalStackContainer("localstack/localstack").withServices("s3")
     }
 
     fun s3Client(localstackContainer: LocalStackContainer): S3Client {
         return S3Client.builder()
-            .endpointOverride(localstackContainer.getEndpointOverride(LocalStackContainer.Service.S3))
+            .endpointOverride(localstackContainer.endpoint)
             .credentialsProvider(
                 StaticCredentialsProvider.create(
                     AwsBasicCredentials.create(localstackContainer.accessKey, localstackContainer.secretKey)
                 )
             )
-            .region(Region.of(localstackContainer.region)).build()
+            .region(Region.of(localstackContainer.region))
+            .build()
     }
 }
