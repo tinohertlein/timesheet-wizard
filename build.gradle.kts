eecc9c451e2e463f2d28cf14871676e477f7e736
Tino Hertlein <tino@hertlein.dev>
2026-08-15 09:18:48 +0200

    build: add Sonargraph plugin configuration for code quality analysis
    - Integrate `com.hello2morrow.sonargraph` plugin version `26.4.1` into the build.
    - Enable automatic updates for Sonargraph.

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,9 +3,14 @@ plugins {
     id("maven-publish")
     alias(libs.plugins.semver)
     alias(libs.plugins.kotlinjvm) apply false
+    id("com.hello2morrow.sonargraph") version "26.4.1"
 }
 
 group = "dev.hertlein.timesheet-wizard"
 description = "The Timesheet Wizard"
 
 version = semver.version
+
+sonargraph {
+    autoUpdate = true
+}
